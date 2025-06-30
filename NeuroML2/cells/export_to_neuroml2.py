78aabe99b935dafb9198b4e88a1bbed7c2605c6e
Hengye <gluciferd@sjtu.edu.cn>
2025-06-30 16:04:53 +0800

    convert morphology

--- NeuroML2/cells/export_to_neuroml2.py
+++ NeuroML2/cells/export_to_neuroml2.py
@@ -26,9 +26,9 @@ def main(acell):
     loader_hoc_file_txt = """
     /*load_file("nrngui.hoc")*/
     load_file("stdrun.hoc")
-    xopen("CT6_reduced_cell.hoc")
+    xopen("ITS4_reduced_cell.hoc")
     objref cell
-    cell = new CT6_reduced_cell()
+    cell = new ITS4_reduced_cell()
     """
 
     with open(loader_hoc_file, 'w') as f:
--- NeuroML2/cells/morph_json2hoc.py
+++ NeuroML2/cells/morph_json2hoc.py
@@ -1,46 +1,55 @@
 import json
+import sys
 
+def json2hoc(acell):
 ### extract morphology data and then make manual adjustments
-with open('CT6_reduced_cellParams.json') as file:
-    data = json.load(file)
+    with open(f"{acell}_reduced_cellParams.json") as file:
+        data = json.load(file)
 
-topol_data = {}
-for sec_name, sec_info in data['secs'].items():
-    if 'topol' in sec_info:
-        topol_data[sec_name] = sec_info['topol']
+    topol_data = {}
+    for sec_name, sec_info in data['secs'].items():
+        if 'topol' in sec_info:
+            topol_data[sec_name] = sec_info['topol']
 
-morpho_data = {}
-for sec_name, sec_info in data['secs'].items():
-    if 'geom' in sec_info and 'pt3d' in sec_info['geom']:
-        morpho_data[sec_name] = sec_info['geom']['pt3d']
+    morpho_data = {}
+    for sec_name, sec_info in data['secs'].items():
+        if 'geom' in sec_info and 'pt3d' in sec_info['geom']:
+            morpho_data[sec_name] = sec_info['geom']['pt3d']
 
-connections = []
-sections = {}
+    connections = []
+    sections = {}
 
-for sec_name, topol_info in topol_data.items():
-    parent_sec = topol_info.get('parentSec')
-    if parent_sec:
-        connections.append((sec_name.strip(), parent_sec))
+    for sec_name, topol_info in topol_data.items():
+        parent_sec = topol_info.get('parentSec')
+        if parent_sec:
+            connections.append((sec_name.strip(), parent_sec))
 
-for sec_name, pt3d_info in morpho_data.items():
-    sections[sec_name] = []
-    for point in pt3d_info:
-        sections[sec_name].append(point)
+    for sec_name, pt3d_info in morpho_data.items():
+        sections[sec_name] = []
+        for point in pt3d_info:
+            sections[sec_name].append(point)
 
-hoc_code = ""
+    hoc_code = ""
 
-hoc_code += "proc topol() { \n"
-for child, parent in connections:
-    hoc_code += f"  connect {child}(0), {parent}(1)\n"
-hoc_code += "}\n\n"
+    hoc_code += "proc topol() { \n"
+    for child, parent in connections:
+        hoc_code += f"  connect {child}(0), {parent}(1)\n"
+    hoc_code += "}\n\n"
 
-for sec_name, pt3d_points in sections.items():
-    hoc_code += f"  {sec_name} {{\n"  
-    hoc_code += "    pt3dclear()\n"
-    for point in pt3d_points:
-        x, y, z, diam = point
-        hoc_code += f"    pt3dadd({x}, {y}, {z}, {diam})\n"  
-    hoc_code += "  }\n"  
+    for sec_name, pt3d_points in sections.items():
+        hoc_code += f"  {sec_name} {{\n"  
+        hoc_code += "    pt3dclear()\n"
+        for point in pt3d_points:
+            x, y, z, diam = point
+            hoc_code += f"    pt3dadd({x}, {y}, {z}, {diam})\n"  
+        hoc_code += "  }\n"  
 
-with open('CT6_reduced_cell.hoc', 'w') as file:
-    file.write(hoc_code)
+    with open(f"{acell}_reduced_cell.hoc", 'w') as file:
+        file.write(hoc_code)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.exit(1)
+    
+    acell = sys.argv[1]
+    json2hoc(acell)
